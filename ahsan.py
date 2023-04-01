ee335a273d0dfd1af936e88e2766b8e8b91cae6e
MDAhsanullah <87916111+MDAhsanullah@users.noreply.github.com>
2023-04-02 03:02:31 +0600

    Update ahsan.py

--- ahsan.py
+++ ahsan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #Open Source Code.No Need More Modification.
-#Code by ErrorX
+#Code by Errorvai
 import sys
 import subprocess
 import os
@@ -18,20 +18,20 @@ import statistics
 import csv
 from typing import Dict
 os.system('cls||clear')
-print('''\033[1;92m❥══════════❥ ↓★↑Cᴏᴅᴇ↓★↑BY↓★↑EʀʀᴏʀX↓★❥══════════❥
+print('''\033[1;92m❥══════════❥ ↓★↑Cᴏᴅᴇ↓★↑BY↓★↑Ahsanullah↓★❥══════════❥
 \033[0m
-\033[1;91m███████╗██╗░░██╗░██╗░░░░░░░██╗██╗███████╗██╗\033[0m
+\033[1;91m██████╗██╗░░██╗░██╗░░░░░░░██╗██╗███████╗██╗\033[0m
 \033[1;92m██╔════╝╚██╗██╔╝░██║░░██╗░░██║██║██╔════╝██║\033[0m
 \033[1;93m█████╗░░░╚███╔╝░░╚██╗████╗██╔╝██║█████╗░░██║\033[0m
 \033[1;94m██╔══╝░░░██╔██╗░░░████╔═████║░██║██╔══╝░░██║\033[0m
 \033[1;95m███████╗██╔╝╚██╗░░╚██╔╝░╚██╔╝░██║██║░░░░░██║\033[0m
 \033[1;96m╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝\033[0m
 ''')
-print('''\033[1;91m❥═════════❥ ↓★↑D̶I̶S̶C̶O̶R̶D̶:̶!ErrorX#0001↓❥═════════❥\033[0m
-\033[1;32mONE LINE COMMAND:\033[0m
-•\033[1;93msudo python ExWiFi/ErrorX.py -i wlan0 -K\033[0m
-\033[1;31mHELP COMMAND:\033[0m
-•\033[1;93mpython ExWiFi/ErrorX.py --help\033[0m
+print('''\033[1;91m❥═════════❥ ↓★↑FB:̶!Amar FB Nai Vai#0001↓❥═════════❥\033[0m
+\033[1;32mjust ONE LINE COMMAND:\033[0m
+•\033[1;93msudo python ahsanwifi/wifimain.py -i wlan0 -K\033[0m
+\033[1;31mHELP!0140332325... COMMAND:\033[0m
+•\033[1;93mpython ahsanwifi/wifimain.py --help\033[0m
 \033[1;95m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[0m''')
 class NetworkAddress:
     def __init__(self, mac):
